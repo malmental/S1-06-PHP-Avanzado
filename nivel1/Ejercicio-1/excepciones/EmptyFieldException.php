1db7514bd4727b8df4c100953d7656e5207c5ee0
malmental <orlandorojasoyarzun@gmail.com>
2025-12-10 18:27:11 +0100

    Se implementa sistema de excepcion emptyfieldexcepcion y y funciona ahora se implementarán las demas

---
+++ nivel1/Ejercicio-1/excepciones/EmptyFieldException.php
@@ -0,0 +1,6 @@
+<?php
+
+class EmptyFieldException extends Exception {
+   
+}
+?>
--- nivel1/Ejercicio-1/procesar.php
+++ nivel1/Ejercicio-1/procesar.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+// Aqui debo nombrarlas en ingles ya que al llamarlas en el codigo, esta mejor de vista
+require_once "excepciones/EmptyFieldException.php";
+require_once "excepciones/EmailException.php";
+require_once "excepciones/NameException.php";
+require_once "excepciones/PhoneException.php";
+
+try {
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        throw new Exception("Acceso negado.");
+    }
+
 
     // La clave del array es el 'name' que pusiste en el HTML.
     $nombre_usuario = $_POST['nombre_usuario'];
@@ -9,26 +20,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $numero_de_telefono = $_POST['numero_de_telefono'];
 
     // Paso 3: mostramos los valores en pantalla
-    echo "<h1>Datos recibidos</h1>";
-    echo "<p>Nombre de usuario: " . $nombre_usuario . "</p>";
-    echo "<p>Correo electronico: " . $correo_electronico . "</p>";
-    echo "<p>Numero de telefono: " . $numero_de_telefono . "</p>";
+    // echo "<h1>Datos recibidos</h1>";
+    // echo "<p>Nombre de usuario: " . $nombre_usuario . "</p>";
+    // echo "<p>Correo electronico: " . $correo_electronico . "</p>";
+    // echo "<p>Numero de telefono: " . $numero_de_telefono . "</p>";
 
+
+    // Verificamos el campo vacio
+    if (empty($nombre_usuario) || empty($correo_electronico) || empty($numero_de_telefono)) {
+        throw new EmptyFieldException( "Todos los campos son requeridos.");
+    }
+
+    if (preg_match("/^[\p{L}' -]+$/u", $nombre_usuario)) {
+        throw new NameException("No puedes ingresar caracteres especiales");
+    }
+
+    if(!filter_var($correo_electronico, FILTER_VALIDATE_EMAIL)) {
+        throw new EmailException("Correo electronico invalido");
+    }
+
+    if(!is_numeric($numero_de_telefono)) {
+        throw new PhoneException ("Debes ingresar numeros");
+    }
+
+
+    // Aqui es donde se guarda la sesion
     $_SESSION['nombre_usuario'] = $nombre_usuario;
     $_SESSION['correo_electronico'] = $correo_electronico;
     $_SESSION['numero_de_telefono'] = $numero_de_telefono;
 
     // Podriamos agregar un mensaje de confirmacion de registro
-    $_SESSION['mensaje_registro_existoso'] = "Genial! $nombre_de_usuario!, tu registro fue exitoso!.";
+    $_SESSION['mensaje_registro_existoso'] = "$nombre_de_usuario!, tu ingreso fue exitoso!.";
 
     echo "<p>Los datos se han guardado con exito!</p>";
     echo "<a href='informacion-usuario.php'>Ver mi perfil</a>";
 
 
-} else {
-
-    echo "<p>Error, debe rellenar todos los campos.</p>";
-    echo "<a href='ejercicio-1-formulario.html'>Volver al formulario</a>";
+} catch (EmptyFieldException $e) {
+    $_SESSION['error'] = $e->getMessage();
 }
 
-?>
+echo "<a href='ejercicio-1-formulario.html'>Volver al formulario</a>";
+    
+    ?>
