2a4e151fb12ba96615eea5fa9fc0408706924d6d
malmental <orlandorojasoyarzun@gmail.com>
2026-01-21 11:17:38 +0100

    ahora es post y edad cambia por email para las validaciones

--- nivel1/Ejercicio-1/session.php
+++ nivel1/Ejercicio-1/session.php
@@ -1,17 +1,19 @@
 <?php 
 session_start();
 
-if (!empty($_POST["Nombre"]) && !empty($_POST["Apellido"]) && !empty($_POST["Edad"]))
+if (!empty($_POST["Nombre"]) && !empty($_POST["Email"]) && !empty($_POST["Edad"]))
 {
-    $nombre = $_GET ["Nombre"];
-    $apellido = $_GET ["Apellido"];
-    $edad = $_GET ["Edad"];
+    $nombre = $_POST ["Nombre"];
+    $email = $_POST ["Email"];
+    $edad = $_POST ["Edad"];
 
     $_SESSION ["Nombre"] = $nombre;
-    $_SESSION ["Apellido"] = $apellido;
+    $_SESSION ["Email"] = $email;
     $_SESSION ["Edad"] = $edad;
 
-    echo "Usuario $nombre, su apellido es $apellido y tiene $edad años" . PHP_EOL;
+    echo 'Usuario: ' . $nombre . PHP_EOL .
+        'Email: ' . $email . PHP_EOL . 
+        'Edad: ' . $edad . PHP_EOL;
     
 } else {
     echo "Los campos no pueden estar vacios";
